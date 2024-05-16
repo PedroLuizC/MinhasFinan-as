49ea657769ffe2280f15f34a5c98a353feb2bc3b
Pedro Luiz <pedrolcavalcanteplcfg.2020@gmail.com>
2024-05-16 13:21:27 -0300

    testando

--- src/app/tab1/tab1.page.spec.ts
+++ src/app/tab1/tab1.page.spec.ts
@@ -1,26 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync, fakeAsync, tick } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
 import { Tab1Page } from './tab1.page';
 
 describe('Tab1Page', () => {
   let component: Tab1Page;
   let fixture: ComponentFixture<Tab1Page>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
       declarations: [Tab1Page],
-      imports: [IonicModule.forRoot(), ExploreContainerComponentModule]
+      imports: [IonicModule.forRoot()]
     }).compileComponents();
 
     fixture = TestBed.createComponent(Tab1Page);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should call openFileSelector() when "Adicionar avatar" button is clicked', fakeAsync(() => {
+    spyOn(component, 'openFileSelector'); // Espionar a função openFileSelector()
+
+    const compiled = fixture.nativeElement;
+    const adicionarAvatarButton = compiled.querySelector('ion-item ion-button'); // Selecionar o botão "Adicionar avatar"
+
+    adicionarAvatarButton.click(); // Simular o clique no botão "Adicionar avatar"
+
+    fixture.detectChanges();
+    tick();
+
+    // Verificar se a função openFileSelector() foi chamada
+    expect(component.openFileSelector).toHaveBeenCalled();
+  }));
 });
--- src/app/tab2/tab2.page.spec.ts
+++ src/app/tab2/tab2.page.spec.ts
@@ -1,26 +1,50 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
+import { AlertController } from '@ionic/angular';
+import { ContaService } from '../../app/services/conta.service';
 import { Tab2Page } from './tab2.page';
 
 describe('Tab2Page', () => {
   let component: Tab2Page;
   let fixture: ComponentFixture<Tab2Page>;
+  let alertControllerSpy: jasmine.SpyObj<AlertController>;
+  let contaServiceSpy: jasmine.SpyObj<ContaService>;
 
   beforeEach(async () => {
+    const alertController = jasmine.createSpyObj('AlertController', ['create']);
+    const contaService = jasmine.createSpyObj('ContaService', ['getSelectedImage', 'contaLogada']);
     await TestBed.configureTestingModule({
       declarations: [Tab2Page],
-      imports: [IonicModule.forRoot(), ExploreContainerComponentModule]
+      imports: [IonicModule.forRoot()],
+      providers: [
+        { provide: AlertController, useValue: alertController },
+        { provide: ContaService, useValue: contaService }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(Tab2Page);
     component = fixture.componentInstance;
+    alertControllerSpy = TestBed.inject(AlertController) as jasmine.SpyObj<AlertController>;
+    contaServiceSpy = TestBed.inject(ContaService) as jasmine.SpyObj<ContaService>;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('calcularTotalExtrato()', () => {
+    it('should return correct total of expenses', () => {
+      component.despesasFixas = [{ nome: 'Despesa 1', valor: 50, data: '2024-05-16', pago: false }, { nome: 'Despesa 2', valor: 100, data: '2024-05-16', pago: false }];
+      component.gastosGerais = [{ nome: 'Gasto 1', valor: 30, data: '2024-05-16', pago: false }, { nome: 'Gasto 2', valor: 70, data: '2024-05-16', pago: false }];
+      const total = component.calcularTotalExtrato();
+      expect(total).toEqual(250); // Usando toEqual() para verificar se o valor retornado é igual a 250
+    });
+
+    it('should return 0 when there are no expenses', () => {
+      const total = component.calcularTotalExtrato();
+      expect(total).toEqual(0); // Usando toEqual() para verificar se o valor retornado é igual a 0
+    });
+  });
+
 });
